a38afff6135c851980c2771a81d77712e517e911
TMad <madsen.tyler@gmail.com>
2018-02-21 11:40:19 -0700

    modify oxford service request based on new classes

--- app/controllers/api/v1/plays_controller.rb
+++ app/controllers/api/v1/plays_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::PlaysController < ApplicationController
   before_action :set_game, only: [:create]
 
   def create
-    if OxfordService.new.word_validation(params[:word]).response.status == 200
+    if OxfordService.new(params[:word]).inflection_response.status == 200
       @game.plays.create!(play_params)
     else
       render json: {"message": "#{params[:word]} is not a valid word."}
