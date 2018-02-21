2bae65a7910dda86881f470d1abc751496bc3bc3
TMad <madsen.tyler@gmail.com>
2018-02-21 10:24:55 -0700

    add games api namespace show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   resources :validates, only: [:index]
+
+  namespace :api do
+    namespace :v1 do
+      resources :games, only: [:show]
+    end
+  end
 end
