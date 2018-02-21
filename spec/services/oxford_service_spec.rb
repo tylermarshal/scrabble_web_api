7880e75454544a1c36de43f4ef1f85219c8dbe8f
TMad <madsen.tyler@gmail.com>
2018-02-21 12:01:27 -0700

    add instance methods for oxford service spec

--- spec/services/oxford_service_spec.rb
+++ spec/services/oxford_service_spec.rb
@@ -25,13 +25,30 @@ describe OxfordService do
         expect(oxford.inflection_response.status).to eq(200)
       end
     end
-#     context "#word_validation" do
-#       it "returns a success message" do
-#         word = "swimmer"
-#         oxford = OxfordService.new(word)
-# require "pry"; binding.pry
-#         expect(oxford.word_validation).to be_successful
-#       end
-#     end
+    context "#word_validation" do
+      it "returns a success message for valid word" do
+        word = "swims"
+        oxford = OxfordService.new(word)
+
+        expect(oxford.word_validation).to eq("'swims' is a valid word and its root form is 'swim'.")
+      end
+    end
+    context "#word_validation" do
+      it "returns a unsuccessful message for unvalid word" do
+        word = "swimz"
+        oxford = OxfordService.new(word)
+
+        expect(oxford.word_validation).to eq("'swimz' is not a valid word.")
+      end
+    end
+    context "#parse" do
+      it "returns a unsuccessful message for unvalid word" do
+        word = "swimmer"
+        oxford = OxfordService.new(word)
+        response = oxford.inflection_response
+
+        expect(oxford.parse(response).class).to eq(Hash)
+      end
+    end
   end
 end
