8040d07164d825602b0ca25f009b99418aacfa47
TMad <madsen.tyler@gmail.com>
2018-02-21 10:51:32 -0700

    add each player total score calculation methods for game

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,4 +6,23 @@ class Game < ApplicationRecord
 
   has_many :plays
 
+  def score_player_1
+    player_1.plays.reduce(Hash.new(0)) do |result, play|
+      unless result.has_key?(result["user_id"])
+        result["user_id"] = play.user_id
+      end
+      result["score"] += play.score
+      result
+    end
+  end
+
+  def score_player_2
+    player_2.plays.reduce(Hash.new(0)) do |result, play|
+      unless result.has_key?(result["user_id"])
+        result["user_id"] = play.user_id
+      end
+      result["score"] += play.score
+      result
+    end
+  end
 end
