c307a78e23f4550eeae17bbe832d0b85955d5ed4
TMad <madsen.tyler@gmail.com>
2018-02-21 11:14:08 -0700

    add start to plays controller

---
+++ app/controllers/api/v1/plays_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::PlaysController < ApplicationController
+
+  before_action :set_game, only: [:show]
+
+  def create
+    @game.plays.create!(play_params)
+  end
+
+  private
+
+    def set_game
+      @game = Game.find(params[:game_id])
+    end
+
+    def play_params
+      params.permit(:user_id, :word)
+    end
+
+end
