e4042faa3aa248d12be47419e9e99efe0d30fefb
TMad <madsen.tyler@gmail.com>
2018-02-21 11:30:02 -0700

    add invalid play request spec

--- spec/requests/plays_request_spec.rb
+++ spec/requests/plays_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "plays API" do
   context "HTTP POST" do
-    it "creats a play for a specific game" do
+    it "creates a play for a specific game" do
       josh = User.create(id: 1, name: "Josh")
       sal = User.create(id: 2, name: "Sal")
 
@@ -27,6 +27,23 @@ describe "plays API" do
       expect(get_game["scores"].first["score"]).to eq(17)
       expect(get_game["scores"].last["user_id"]).to eq(2)
       expect(get_game["scores"].last["score"]).to eq(16)
+    end
+    it "stops and invalid play from being made" do
+      josh = User.create(id: 1, name: "Josh")
+      sal = User.create(id: 2, name: "Sal")
+
+      game = Game.create(player_1: josh, player_2: sal)
+
+      josh.plays.create(game: game, word: "sal", score: 3)
+      josh.plays.create(game: game, word: "zoo", score: 12)
+      sal.plays.create(game: game, word: "josh", score: 14)
+      sal.plays.create(game: game, word: "no", score: 2)
+
+      post "/api/v1/games/#{game.id}/plays?user_id=1&word=foxez"
+
+      message = JSON.parse(response.body)
+      
+      expect(message).to eq({"message": "foxez is not a valid word."})
     end
   end
 end
