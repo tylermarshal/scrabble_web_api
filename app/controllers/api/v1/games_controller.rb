c774fd89fc582857e41aea8bc9e925e61294316b
TMad <madsen.tyler@gmail.com>
2018-02-21 10:37:45 -0700

    add before action and render json to games controller

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::GamesController < ApplicationController
 
+  before_action :set_game, only: [:show]
+  
   def show
-    
+    render json: @game, each_serializer: GameSerializer
   end
+
+  private
+
+    def set_game
+      @game = Game.find(params[:id])
+    end
 
 end
