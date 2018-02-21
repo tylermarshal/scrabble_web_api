303c466672fcdb952096a77f37dea00e71328911
TMad <madsen.tyler@gmail.com>
2018-02-21 10:38:14 -0700

    add active model serialization include to game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,9 @@
 class Game < ApplicationRecord
+  include ActiveModel::Serialization
+
   belongs_to :player_1, class_name: "User", foreign_key: :player_1_id
   belongs_to :player_2, class_name: "User", foreign_key: :player_2_id
 
   has_many :plays
+
 end
