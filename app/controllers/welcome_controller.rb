3bf3547be4b5f82d3f87965efa2cf7c2a0836b23
TMad <madsen.tyler@gmail.com>
2018-02-27 18:52:55 -0700

    send object to view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    if params[:enter_word]
+      @validation_response = OxfordService.new(params[:enter_word]).word_validation
+    end
   end
 end
