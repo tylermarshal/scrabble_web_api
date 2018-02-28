64974f5fc976bc564867f8499a335ac5ec30fd2e
TMad <madsen.tyler@gmail.com>
2018-02-27 18:51:48 -0700

    remove validations controller and routes

--- app/controllers/validates_controller.rb
+++
@@ -1,6 +0,0 @@
-class ValidatesController < ApplicationController
-  def index
-    flash[:notice] = OxfordService.new(params[:enter_word]).word_validation
-    redirect_to root_path
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root "welcome#index"
 
-  resources :validates, only: [:index]
-
   namespace :api do
     namespace :v1 do
       resources :games, only: [:show] do
